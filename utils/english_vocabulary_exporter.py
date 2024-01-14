1eb4c4314b623cff1b3b546d01d886df8b9d20f2
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2024-01-14 10:33:36 +0100

    prepare ground for EN vocab. exporter script

---
+++ utils/english_vocabulary_exporter.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import argparse
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
